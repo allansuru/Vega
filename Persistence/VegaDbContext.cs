570c94f235e952fce411098f284c1fa070075656
Allan SuRu <allansuru@hotmail.com>
2017-10-19 20:07:30 -0200

    - Add uma nova classe VehicleFeature - No dbContext, repare nas chaves pra ficar mts pra mts - Zenconding exemplo mult - Adicionei classe, usei migration -

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -9,15 +9,20 @@ namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        // como na classe Make, eu tenho a Model, o entity framework ja se liga nisso e cria as tabelas relacionadas
+        //public DbSet<Model> Models { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options )
             :base(options)
         {
-
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+              new { vf.VehicleId, vf.FeatureId });
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature>  Features { get; set; }
-        // como na classe Make, eu tenho a Model, o entity framework ja se liga nisso e cria as tabelas relacionadas
-        //public DbSet<Model> Models { get; set; }
+
     }
 }
