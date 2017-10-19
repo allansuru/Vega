24e63fad878622464775143d991652f29e000842
Allan SuRu <allansuru@hotmail.com>
2017-10-19 20:31:42 -0200

    - Adicionando a api/vehicles (Controller) - Arrumando os Mappings - Add o dbcontext

--- Controllers/VehiclesController .cs
+++ Controllers/VehiclesController .cs
@@ -1,7 +1,10 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 using Vega.Controllers.Resources;
 using Vega.Models;
+using Vega.Persistence;
 
 namespace vega.Controllers
 {
@@ -9,16 +12,25 @@ namespace vega.Controllers
     public class VehiclesController : Controller
     {
         private readonly IMapper mapper;
-        public VehiclesController(IMapper mapper)
+        private readonly VegaDbContext context;
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
+            this.context = context;
             this.mapper = mapper;
         }
 
         [HttpPost]
-        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
         }
     }
 }
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -11,6 +11,7 @@ namespace Vega.Persistence
     {
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
         // como na classe Make, eu tenho a Model, o entity framework ja se liga nisso e cria as tabelas relacionadas
         //public DbSet<Model> Models { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options )
