bc441a5d233bb6a61c6e998af3e8aaf9fbe0c38e
Allan SuRu <allansuru@hotmail.com>
2017-10-22 19:29:57 -0200

    - Adicionando e entendendo ModelState, adicionando validação. - Adicionando no objeto do VehicleResouce o lastUpdate, lembrando q sempre tem q ta no resource e na model, pra funfar direito =)

--- Controllers/VehiclesController .cs
+++ Controllers/VehiclesController .cs
@@ -22,6 +22,19 @@ namespace vega.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var model = await context.Models.FindAsync(vehicleResource.ModelId);
+
+            if(model == null)
+            {
+                ModelState.AddModelError("modelId", "Invalid Model ID");
+                return BadRequest(ModelState);
+            }
+
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
